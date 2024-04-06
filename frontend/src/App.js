9b27222d199a879b15a311156ce94b47705b4f15
sai-na <saipkmm100@gmail.com>
2024-04-07 01:06:49 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <>
+                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+              </>
+            </>
+          }
+        />
+      </Routes>{" "}
+    </Router>
+  )
 }
-
 export default App
