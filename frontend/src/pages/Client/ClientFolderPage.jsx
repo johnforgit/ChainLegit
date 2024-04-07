f4510e73a44db2580896ae888557bd6d695608ab
sai-na <saipkmm100@gmail.com>
2024-04-07 23:27:28 +0530

    folder

---
+++ frontend/src/pages/Client/ClientFolderPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Folder from '../../components/Folder'
+import Title from '../../components/Title'
+
+function ClientFolderPage() {
+  return (
+    <div className='mt-16 w-full'>
+        <Title>eee</Title>
+  
+    <div className='mt-16 flex   mx-10'>
+   
+
+        <Folder/>
+        <Folder/>
+        <Folder/>
+        <Folder/>
+        <Folder/>
+        <Folder/>
+    </div></div>
+  )
+}
+
+export default ClientFolderPage
